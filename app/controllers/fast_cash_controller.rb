87ca33f3801993e75eb2dda344c4513732d38292
panhara <titpanhara@gmail.com>
2019-11-06 11:40:24 +0700

    modified:   app/controllers/fast_cash_controller.rb 	modified:   app/views/fast_cash/index.html.erb 	modified:   app/views/sales/_order_item_row.html.erb

--- app/controllers/fast_cash_controller.rb
+++ app/controllers/fast_cash_controller.rb
@@ -15,12 +15,12 @@ class FastCashController < ApplicationController
   end
 
   def set_manual_cash_usd
-    current_order.update(cash: params[:usd])
+    current_order.update(cash: params[:usd], cash_in_riel: 0)
     redirect_to sales_path
   end
 
   def set_manual_cash_riel
-    current_order.update(cash_in_riel: params[:riel])
+    current_order.update(cash_in_riel: params[:riel], cash: 0)
     redirect_to sales_path
   end
 
