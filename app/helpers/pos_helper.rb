bf24170b031abe89e440bceae4bd19b848ee9271
panhara <titpanhara@gmail.com>
2019-11-18 14:19:26 +0700

    fix everthing

--- app/helpers/pos_helper.rb
+++ app/helpers/pos_helper.rb
@@ -1,13 +1,13 @@
 module PosHelper
 
   def display_change_in_usd (order)
-    number_to_currency ((order.total_cash_in_riel - order.total * Constant::exchange_rate) / Constant::exchange_rate).to_i, precision: 0
+    number_to_currency ((order.total_cash_in_riel - order.total * Constant::exchange_rate) / Constant::exchange_rate).to_d, precision: 0
   end
 
   def display_change_in_riel (order)
     n = (order.total_cash_in_riel - order.total * Constant::exchange_rate) / Constant::exchange_rate
     remain = n.to_s.split('.')
-    return ('0.' + remain[1]).to_f * Constant::exchange_rate
+    return ('0.'.to_f + remain[1].to_f) * Constant::exchange_rate
   end
 
 end
