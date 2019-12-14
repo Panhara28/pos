21cba354cd30364b9422750f24d09d719a3819cb
Panhara28 <titpanhara@gmail.com>
2019-12-14 16:28:58 +0700

    fix cashdrawer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191209054809) do
+ActiveRecord::Schema.define(version: 20191214072702) do
 
   create_table "admins", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "email", default: "", null: false
@@ -162,6 +162,7 @@ ActiveRecord::Schema.define(version: 20191209054809) do
     t.decimal "delivery_fee", precision: 12, scale: 3
     t.integer "delivery_id"
     t.decimal "profit", precision: 12, scale: 3
+    t.text "note"
     t.index ["customer_id"], name: "index_orders_on_customer_id"
     t.index ["seat_table_id"], name: "index_orders_on_seat_table_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
