964393e4288ba7ee64e872aa02e7035e52b4a3fe
Panhara28 <titpanhara@gmail.com>
2020-01-11 22:16:07 +0700

    fix

--- app/models/order.rb
+++ app/models/order.rb
@@ -44,7 +44,8 @@ class Order < ApplicationRecord
 
   private
     def update_subtotal
-      self[:subtotal] = subtotal + (subtotal * Constant::vat / 100)
+      puts "C: #{Constant::vat.present?}"
+      self[:subtotal] = subtotal + (subtotal * Constant::vat.nil? ? 0.0 : Constant::vat / 100)
       # puts "DF: #{self[:delivery_fee].nil? ? 0:(subtotal * (self[:delivery_fee] / 100))}"
       self[:total] = subtotal + (subtotal * self[:tax] / 100) - (subtotal * (self[:discount] / 100))
     end
--- db/migrate/20191209054028_add_vat_to_exchange_rates.rb
+++ db/migrate/20191209054028_add_vat_to_exchange_rates.rb
@@ -1,5 +1,5 @@
 class AddVatToExchangeRates < ActiveRecord::Migration[5.1]
   def change
-    add_column :exchange_rates, :vat, :decimal, precision: 12, scale: 3
+    add_column :exchange_rates, :vat, :decimal, precision: 12, scale: 3, default: 0.0
   end
 end
