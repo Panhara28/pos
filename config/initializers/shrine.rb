679f106bd67b042955c335951e961646daa37b00
Panhara28 <titpanhara@gmail.com>
2020-01-15 12:27:10 +0700

    fix env

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -1,16 +1,16 @@
 require 'shrine/storage/s3'
 
 s3_options = {
-  access_key_id: "2J4SKHSCRQZJ5RQQKNL5",
-  secret_access_key: "D9x/AFEfIptuY8THn5sYSQKF36TBeuWkVP7W24K98K8",
-  bucket: "image-pos",
-  endpoint: 'https://sgp1.digitaloceanspaces.com',
-  region: 'isgp1'
+  access_key_id: ENV['ACCESS_KEY'],
+  secret_access_key: ENV['SECRET'],
+  bucket: ENV['BUCKET'],
+  endpoint: ENV['ENDPOINT'],
+  region: ENV['REGION']
 }
 
 Shrine.storages = {
   cache: Shrine::Storage::S3.new(prefix: 'cache', upload_options: {acl: 'public-read'}, **s3_options),
-  store: Shrine::Storage::S3.new(prefix: 'products', upload_options: {acl: 'public-read'}, **s3_options),
+  store: Shrine::Storage::S3.new(prefix: ENV['SERVER_IMAGE'], upload_options: {acl: 'public-read'}, **s3_options),
 }
 
 Shrine.plugin :activerecord
